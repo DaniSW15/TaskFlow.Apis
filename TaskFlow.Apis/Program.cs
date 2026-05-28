90b3f152c4278f7f3d04fec9218f5e10a27e6128
Daniel CG <daniel.cg15.tab@gmail.com>
2026-05-27 20:46:27 -0600

    Add .gitignore file to exclude build and temporary files

---
+++ TaskFlow.Apis/Program.cs
@@ -0,0 +1,16 @@
+using TaskFlow.Application;
+using TaskFlow.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.MapGet("/health", () => Results.Ok(new { status = "Healthy" }))
+    .WithName("HealthCheck");
+
+app.Run();
